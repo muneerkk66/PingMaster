12a53e0a12052e4a57c6c695d111e4e20e12297c
Muneer Kalakuzhiyil <muneerkk66@gmail.com>
2024-04-13 11:26:25 +0100

    Initial Commit

---
+++ PingApp/PingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingAppApp.swift
+//  PingApp
+//
+//  Created by Muneer K K on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
