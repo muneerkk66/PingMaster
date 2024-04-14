e45e98fa83a9a358e67995c9cc779c5e43fc6b2a
Muneer Kalakuzhiyil <muneerkk66@gmail.com>
2024-04-14 01:31:39 +0100

    test: integrate snapshot testing

--- PingAppTests/PingAppTests.swift
+++ PingAppTests/PingAppTests.swift
@@ -6,8 +6,10 @@
 //
 
 import XCTest
+import SnapshotTesting
 @testable import PingApp
 
+@MainActor
 final class PingAppTests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -18,12 +20,25 @@ final class PingAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testDefaultAppearanceWithSuccess ( ) {
+        let contentView = HomeView(viewModel: HomeViewModel(coordinator: MockHomeCoordinator(), fetchHostsUseCase: MockFetchAllHostsUseCase.success(with: [MockResponse.hostResponse]), findLatencyUseCase: MockFindLatencyUseCase.success(with: MockResponse.latencyResponse)))
+
+        assertSnapshot(of: contentView.toVC(), as: .image)
+    }
+
+    func testDefaultAppearanceWithError ( ) {
+        let contentView = HomeView(viewModel: HomeViewModel(coordinator: MockHomeCoordinator(), fetchHostsUseCase: MockFetchAllHostsUseCase.failure(error: APIError.applicationError), findLatencyUseCase: MockFindLatencyUseCase.failure(error: APIError.applicationError)))
+
+        assertSnapshot(of: contentView.toVC(), as: .image)
+    }
+
+	func testDetailView ( ) {
+		let contentView = DetailView(latency: "Test")
+		assertSnapshot(of: contentView.toVC(), as: .image)
+	}
+    func testErroView() {
+        let errorView = ErrorView(errorMessage: "error") {}
+        assertSnapshot(of: errorView.toVC(), as: .image)
     }
 
     func testPerformanceExample() throws {
